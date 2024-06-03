3aa7f92623ea5c76ee751c6eff479e68bf24a322
j3lte <jwlagendijk@gmail.com>
2024-06-03 18:13:12 +0200

    Add some comments

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
 import type { Message } from "protobufjs";
 import type { Buffer } from "node:buffer";
 
+/**
+ * CastV2 message types
+ */
 export type CastMessage =
   | "CastMessage"
   | "AuthChallenge"
@@ -8,32 +11,84 @@ export type CastMessage =
   | "AuthError"
   | "DeviceAuthMessage";
 
+/**
+ * CastV2 message methods
+ */
 export type MessageMethods = {
+  /**
+   * Serialize a message
+   * @param data - message data
+   * @returns serialized message
+   */
   serialize: (data: Message) => Uint8Array;
+  /**
+   * Parse a message
+   * @param data - serialized message
+   * @returns parsed message
+   */
   parse: (data: Uint8Array) => Message;
 };
 
+/**
+ * CastV2 message exports
+ */
 export type MessageExport = Record<CastMessage, MessageMethods>;
 
+/**
+ * CastV2 message
+ */
 export interface SendMessage {
+  /**
+   * Protocol version
+   */
   protocolVersion: number;
+  /**
+   * Source ID
+   */
   sourceId: string;
+  /**
+   * Destination ID
+   */
   destinationId: string;
+  /**
+   * Namespace
+   */
   namespace: string;
+  /**
+   * Payload type
+   *
+   * 0: string
+   * 1: binary
+   */
   payloadType: number;
+  /**
+   * Binary payload
+   */
   payloadBinary?: Buffer;
+  /**
+   * UTF-8 payload
+   */
   payloadUtf8?: string;
 }
 
+/**
+ * Packet stream wrapper events
+ */
 export type PacketStreamWrapperEvents = {
   packet: [Uint8Array];
 };
 
+/**
+ * Channel events
+ */
 export type ChannelEvents = {
   message: [unknown, boolean];
   close: [];
 };
 
+/**
+ * Client event types
+ */
 export type ClientEventTypes = {
   connect: [];
   close: [];
@@ -41,6 +96,9 @@ export type ClientEventTypes = {
   message: [string, string, string, string | Buffer];
 };
 
+/**
+ * Client connect options
+ */
 export interface ClientConnectOptions {
   host: string;
   port?: number;
